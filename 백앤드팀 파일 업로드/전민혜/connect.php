a7de970e540768fe53ed24bbf0c13e84a7022770
PUSH <pushpull7381@gmail.com>
2021-01-14 05:15:00 +0900

    2

--- 백앤드팀 파일 업로드/전민혜/connect.php
+++ 백앤드팀 파일 업로드/전민혜/connect.php
@@ -1,10 +1,10 @@
 <?php
 $db_host="localhost";
 $db_user="root";
-$db_password="push1108";
+$db_password="Mine_7381";
 $db_name="mine";
 
-$conn = mysqli_connect('localhost','root','push1108','mine');
+$conn = mysqli_connect('localhost','root','Mine_7381','mine');
 $con = new mysqli($db_host, $db_user, $db_password, $db_name);
 if ($con->connect_errno) { die('Connection Error : '.$con->connect_error);}
 ?>
